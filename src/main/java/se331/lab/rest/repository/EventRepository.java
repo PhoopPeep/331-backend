34a1755b9d0f006a9d2724d460289b419fa4d7e2
PhoopPeep <christina_c@cmu.ac.th>
2024-10-10 21:28:33 +0700

    lab10 1.4

--- src/main/java/se331/lab/rest/repository/EventRepository.java
+++ src/main/java/se331/lab/rest/repository/EventRepository.java
@@ -5,8 +5,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import se331.lab.rest.entity.Event;
 
-import java.util.List;
-
 public interface EventRepository extends JpaRepository<Event, Long> {
     Page<Event> findByTitle(String title, Pageable pageRequest);
+    Page<Event> findByTitleContaining(String title, Pageable pageRequest);
 }
